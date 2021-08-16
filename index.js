761c07c9c5a37e56bb7ff1b90921ef291ce01059
RITESH MAHOUR <riteshmahour94@gmail.com>
2021-08-16 17:44:58 +0530

    changes in login file

--- index.js
+++ index.js
@@ -1,119 +1,117 @@
-const express=require("express")
-const app=express()
-const mysql=require('mysql')
-const bodyParser=require('body-parser')
-const path=require('path')
+const express = require("express")
+const app = express()
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
+const path = require('path')
 const { parse } = require("path")
-const joi=require("@hapi/joi")
+const joi = require("@hapi/joi")
 const { array } = require("@hapi/joi")
-const bcrypt=require("bcrypt")
+const bcrypt = require("bcrypt")
 
 
 
 // Forn ejs engine
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 
 // add style into the file
 
-app.use('/style',express.static(path.join(__dirname,'style')))
+app.use('/style', express.static(path.join(__dirname, 'style')))
 
 
 // BodyParser
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
 // Redirect Pages
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,"files","home.html"))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "files", "home.html"))
 })
-app.get('/login.html',(req,res)=>{
-    res.sendFile(path.join(__dirname,"files","login.html"))
+app.get('/login.html', (req, res) => {
+    res.sendFile(path.join(__dirname, "files", "login.html"))
 })
-app.get('/register.html',(req,res)=>{
-    res.sendFile(path.join(__dirname,"files","register.html"))
+app.get('/register.html', (req, res) => {
+    res.sendFile(path.join(__dirname, "files", "register.html"))
 })
-app.get('/home.html',(req,res)=>{
-    res.sendFile(path.join(__dirname,"files","home.html"))
+app.get('/home.html', (req, res) => {
+    res.sendFile(path.join(__dirname, "files", "home.html"))
 })
 app.listen(3000)
 
 
 // Database
 
-const connection=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:"webAuth",
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: "webAuth",
 
 })
 
-connection.connect((err)=>{
-    if(!!err){
+connection.connect((err) => {
+    if (!!err) {
         console.log(err)
     }
-    else{
+    else {
         console.log('connected')
     }
 })
 
-app.post('/register.html',(req,res)=>{
-    let hashPass=bcrypt.hashSync(req.body.password,14)
+app.post('/register.html', (req, res) => {
+    let hashPass = bcrypt.hashSync(req.body.password, 14)
     // console.log(hashPass)
-    const sql=`INSERT into user SET?`;
-    const data={
-        name:`${req.body.name}`,
-        email:`${req.body.email}`,
-        password:`${hashPass}`,
-        phNumber:`${req.body.phNumber}`
+    const sql = `INSERT into user SET?`;
+    const data = {
+        name: `${req.body.name}`,
+        email: `${req.body.email}`,
+        password: `${hashPass}`,
+        phNumber: `${req.body.phNumber}`
     }
 
-    connection.query(sql,data,(err,result)=>{
-       const parsed= JSON.parse(JSON.stringify(data))
-    //    console.log(parsed,"parsed data")
-        if(err){
-            let errr="Somthing went wrong"
+    connection.query(sql, data, (err, result) => {
+        const parsed = JSON.parse(JSON.stringify(data))
+        //    console.log(parsed,"parsed data")
+        if (err) {
+            let errr = "Somthing went wrong"
         }
-        else{
+        else {
             console.log("successfully sent data into DB")
             // console.log()
-           res.render("dashboard",[dataa=parsed])
+            res.render("dashboard", [dataa = parsed])
         }
     })
 })
 
-app.post('/login.html',(req,res)=>{
-    const slct_all=`SELECT * from user;`
+app.post('/login.html', (req, res) => {
+    const slct_all = `SELECT * from user;`
     // const slct_data=`SELECT * from user WHERE password="${req.body.password}" AND email="${req.body.email}"`;
-connection.query(slct_all,(err,result)=>{
-    if(err)
-    {
-        let errr="something went wrong"
-    }
-    else{
-        console.log(result,"result")
-        var bool;
-        var verifyUser;
-        result.forEach(element => {
-             verifyUser= JSON.parse(JSON.stringify(element))
-        
-            if(verifyUser.email===req.body.email && bcrypt.compareSync(req.body.password,verifyUser.password)){
-                res.render('dashboard',[dataa=verifyUser]) 
-                bool=true
-            }
-            if(verifyUser.email!=req.body.email || verifyUser.password!=req.body.password){
-              bool=false    
-            }
-            
-        });
-        if(!bool)
-        {
-            res.render('login',[er="incorrect email/password"])
+    connection.query(slct_all, (err, result) => {
+        if (err) {
+            let errr = "something went wrong"
         }
-    }
+        else {
+            console.log(result, "result")
+            var bool;
+            var verifyUser;
+            result.forEach(element => {
+                verifyUser = JSON.parse(JSON.stringify(element))
+
+                if (verifyUser.email === req.body.email && bcrypt.compareSync(req.body.password, verifyUser.password)) {
+                    res.render('dashboard', [dataa = verifyUser])
+                    bool = true
+                }
+                if (verifyUser.email != req.body.email || verifyUser.password != req.body.password) {
+                    bool = false
+                }
+
+            });
+            if (!bool) {
+                res.render('login', [er = "incorrect email/password"])
+            }
+        }
 
 
-})
-  
+    })
+
 })
